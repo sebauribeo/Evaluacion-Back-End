5b32ddd9064b66516592566dd187ea79e01cd5f9
sebauribeo <chefsebauribe@gmail.com>
2022-09-13 10:13:04 -0300

    update templates

--- evaluacion/views.py
+++ evaluacion/views.py
@@ -4,17 +4,15 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'home.html')
 
-def vista1(request):
-    return render(request, 'vista1.html')
+def productos(request):
+    return render(request, 'productos.html')  
 
-def vista2(request):
-    return render(request, 'vista2.html')
+def contacto(request):
+    return render(request, 'contacto.html')
 
-def vista3(request):
-    return render(request, 'vista3.html')
+def inventario(request):
+    return render(request, 'inventario.html')
 
-def vista4(request):
-    return render(request, 'vista4.html')
+def admin(request):
+    return render(request, 'admin.html')
 
-def vista5(request):
-    return render(request, 'vista5.html')
--- evaluacion_1/urls.py
+++ evaluacion_1/urls.py
@@ -3,9 +3,8 @@ from evaluacion import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
-    path('vista1', views.vista1, name = 'vista1'),
-    path('vista2', views.vista2, name = 'vista2'),
-    path('vista3', views.vista3, name = 'vista3'),
-    path('vista4', views.vista4, name = 'vista4'),
-    path('vista5', views.vista5, name = 'vista5'),
+    path('productos', views.productos, name = 'productos'),
+    path('contacto', views.contacto, name = 'contacto'),
+    path('inventario', views.inventario, name = 'inventario'),
+    path('admin', views.admin, name = 'admin'),
 ]
